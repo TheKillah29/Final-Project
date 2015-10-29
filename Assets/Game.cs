1fa4c18965d713d6a958d6bb0cb59b7e47e2c63c
David Reynolds <david.reynolds101a@gmail.com>
2015-10-29 17:49:06 -0400

    Final Project Game
    Made the repository

---
+++ Assets/Game.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+
+	public GameObject[] GameObjects;
+
+    protected static string mText;
+    public static string MyText
+    {
+        get { return mText; }
+        set { mText = value; }
+    }
+
+    void Awake ()
+    {
+         GameObjects = GameObject.FindObjectsOfType(typeof(GameObject)) as GameObject[];
+         for (int i= 0; i< GameObjects.Length; i++)
+         {
+              MyText+= GameObjects[i].name + "-";
+         }
+    }
+
+    void Update()
+    {
+         if(Input.GetButtonDown("Jump"))
+         {
+             Debug.Log(MyText);
+         }
+    }
+}
---
+++ Assets/Problem.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Problem : MonoBehaviour {
+
+    // Use for initialization
+    void Start()
+    {
+        Game.MyText = "nope.";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
